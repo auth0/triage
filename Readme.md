ca4d6f90936040f396fccf6a60b075805922ab6b
Duncan Godfrey <duncan@auth0.com>
2017-04-11 08:35:47 -0400

    Update the README and add jshint file

--- Readme.md
+++ Readme.md
@@ -1,2 +1,55 @@
-# Triage
+# README
 
+## Overview
+
+Triage is a [Slash Webtask](https://webtask.io/slack) that we use to triage our #security-alerts Slack channel.
+
+We took inspiration from the Slack security team ( [talk](https://www.youtube.com/watch?v=Xh-aB8Im_Ys) ) who were kind enough to open source their [code](https://github.com/johnagan/triagebot-example).
+
+After reviewing their example code we realized it was a perfect fit for our own [Webtask](https://webtask.io/) service.
+
+It can run it two modes:
+
+* From within a channel: it will triage the alerts in the channel
+* From a DM: when a channel name is passed included in the call it will triage that channel
+
+## Installation
+
+The requirements for Triage are a Slack bot token and the Slash Webtasks Slack app.
+
+1. Follow the instructions [here](https://webtask.io/slack) to enable Slash Webtasks for your Slack organization.
+1. After it's installed you create a new Slash Webtask by running `/wt make triage` and then `wt edit triage` to open the Webtask editor.
+1. Add your Slack bot token as a [secret](https://webtask.io/docs/editor/secrets) called `SLACK_TOKEN` and copy over the code from this repository.
+1. It can be run from within a channel `/wt triage` or from a DM `/wt triage #security-alerts`.
+
+## What is Auth0?
+
+Auth0 helps you to:
+
+* Add authentication with [multiple authentication sources](https://docs.auth0.com/identityproviders), either social like **Google, Facebook, Microsoft Account, LinkedIn, GitHub, Twitter, Box, Salesforce, among others**, or enterprise identity systems like **Windows Azure AD, Google Apps, Active Directory, ADFS or any SAML Identity Provider**.
+* Add authentication through more traditional **[username/password databases](https://docs.auth0.com/mysql-connection-tutorial)**.
+* Add support for **[linking different user accounts](https://docs.auth0.com/link-accounts)** with the same user.
+* Support for generating signed [Json Web Tokens](https://docs.auth0.com/jwt) to call your APIs and **flow the user identity** securely.
+* Analytics of how, when and where users are logging in.
+* Pull data from other sources and add it to the user profile, through [JavaScript rules](https://docs.auth0.com/rules).
+
+
+## Create a free account in Auth0
+
+1. Go to [Auth0](https://auth0.com) and click Sign Up.
+2. Use Google, GitHub or Microsoft Account to login.
+
+
+## Issue Reporting
+
+If you have found a bug or if you have a feature request, please report them at this repository issues section. Please do not report security vulnerabilities on the public GitHub issue tracker. The [Responsible Disclosure Program](https://auth0.com/whitehat) details the procedure for disclosing security issues.
+
+
+## Author
+
+[Auth0](https://auth0.com)
+
+
+## License
+
+This project is licensed under the MIT license. See the [LICENSE](LICENSE) file for more info.
--- webtask.js
+++ webtask.js
@@ -38,6 +38,127 @@ const DEFAULTS = {
 };
 
 
+function getChannelIdByName(slackToken, name) {
+  const slackChannelsListURL = 'https://slack.com/api/channels.list';
+  const params = qs.stringify({
+    token: slackToken
+  });
+
+  return http.post(slackChannelsListURL, params)
+    .then((resp) => {
+      if (!resp.data.ok) {
+        console.log(resp.data.error);
+        return null;
+      }
+
+      const channel = resp.data.channels.filter(
+        cn => cn.name.toLowerCase() === name.replace('#', '').toLowerCase()
+      );
+
+      if (channel.length === 1) {
+        return channel.pop()['id'];
+      }
+
+      return null;
+    });
+}
+
+function getSlackHistory(slackToken, channelId, historySize) {
+  const slackHistoryURL = 'https://slack.com/api/channels.history';
+  const params = qs.stringify({
+    count: historySize,
+    token: slackToken,
+    channel: channelId
+  });
+
+  // Return Promise
+  return http.post(slackHistoryURL, params);
+}
+
+
+function getRequest(settings, message) {
+  // the emoji that was matched
+  let test = new RegExp(settings.pending.emojis.join('|'));
+  let match = message.text.match(test);
+  let emoji = match ? match[0] : null;
+
+  // flags based on reactions
+  let reactions = (message.reactions || []).map(r => r.name);
+  let addressed = settings.addressed.emojis.some(e => _.includes(reactions, e));
+  let review = settings.review.emojis.some(e => _.includes(reactions, e)) && !addressed;
+  let pending = emoji && !review && !addressed;
+
+  let id = message.ts.replace('.', '');
+  let bot = settings.skip_bots && (message.subtype === 'bot_message');
+  let priority = settings.pending.emojis.indexOf(emoji);
+
+  return { bot, priority, emoji, addressed, pending, review, id, message };
+}
+
+
+function buildSection(settings, requests, payload, name) {
+  let channel_id = payload.channel_id;
+  let team_domain = payload.team_domain;
+  let channel_name = payload.channel_name;
+
+  // Correct the url if this comes from a DM
+  if (payload.channel_name === directMessageHandle) {
+    channel_name = payload.text;
+  }
+
+  let baseUrl = `https://${team_domain}.slack.com/archives/${channel_name}/p`;
+
+  let title = settings[name].title;
+  let filtered = requests.filter(r => r[name]);
+  let items = filtered.map(r => `:${r.emoji}: ${baseUrl + r.id}`);
+  let text = [title].concat(items).join('\n');
+
+  // replace template fields
+  text = text.replace(/{{count}}/, filtered.length);
+  text = text.replace(/{{channel}}/, `<#${channel_id}|${channel_name}>`);
+
+  return text;
+}
+
+
+function buildMessage(payload, requests, settings) {
+  let message = { unfurl_links: settings.unfurl_links };
+  let publish_test = new RegExp(settings.publish_text, 'i');
+
+  // build display text
+  let map = buildSection.bind(null, settings, requests, payload);
+  message.text = settings.display.map(map).join('\n\n\n');
+
+  // attach instructions if not publish else make public
+  if (publish_test.test(payload.text)) message.response_type = 'in_channel';
+  else message.attachments = settings.help;
+
+  return message;
+}
+
+function create(payload, messages, options) {
+  let settings = Object.assign({}, DEFAULTS, options);
+
+  let map = getRequest.bind(null, settings);
+  let sort = (a, b) => a.priority - b.priority;
+  let filter = m => m.emoji && !m.bot;
+
+  let requests = messages.map(map).filter(filter).sort(sort);
+  let message = buildMessage(payload, requests, settings);
+
+  return message;
+}
+
+
+function postResults(payload, result) {
+  return http.post(payload.response_url, result);
+}
+
+function sync_response(cb, text, only_caller) {
+  cb(null, { text: text, response_type: only_caller ? 'emphemeral' : 'in_channel' });
+}
+
+
 module.exports = (ctx, cb) => {
   const HISTORY_SIZE = 1000;
   const SLACK_TOKEN = ctx.secrets.SLACK_TOKEN;
@@ -73,118 +194,3 @@ module.exports = (ctx, cb) => {
     });
   }, startTimeout);
 };
-
-function getChannelIdByName(slackToken, name) {
-  const slackChannelsListURL = 'https://slack.com/api/channels.list';
-  const params = qs.stringify({
-    token: slackToken
-  });
-
-  return http.post(slackChannelsListURL, params)
-    .then((resp) => {
-      if (!resp.data.ok) {
-        console.log(resp.data.error);
-        return null;
-      }
-      const channel = resp.data.channels.filter(cn => cn.name.toLowerCase() === name.replace('#', '').toLowerCase());
-
-      if (channel.length === 1) {
-        return channel.pop()['id'];
-      }
-
-      return null;
-    });
-}
-
-function getSlackHistory(slackToken, channelId, historySize) {
-  const slackHistoryURL = 'https://slack.com/api/channels.history';
-  const params = qs.stringify({
-    count: historySize,
-    token: slackToken,
-    channel: channelId
-  });
-
-  // Return Promise
-  return http.post(slackHistoryURL, params);
-}
-
-function create(payload, messages, options) {
-  let settings = Object.assign({}, DEFAULTS, options);
-
-  let map = getRequest.bind(null, settings);
-  let sort = (a, b) => a.priority - b.priority;
-  let filter = m => m.emoji && !m.bot;
-
-  let requests = messages.map(map).filter(filter).sort(sort);
-  let message = buildMessage(payload, requests, settings);
-
-  return message;
-}
-
-function getRequest(settings, message) {
-  // the emoji that was matched
-  let test = new RegExp(settings.pending.emojis.join('|'));
-  let match = message.text.match(test);
-  let emoji = match ? match[0] : null;
-
-  // flags based on reactions
-  let reactions = (message.reactions || []).map(r => r.name);
-  let addressed = settings.addressed.emojis.some(e => _.includes(reactions, e));
-  let review = settings.review.emojis.some(e => _.includes(reactions, e)) && !addressed;
-  let pending = emoji && !review && !addressed;
-
-  let id = message.ts.replace('.', '');
-  let bot = settings.skip_bots && (message.subtype === 'bot_message');
-  let priority = settings.pending.emojis.indexOf(emoji);
-
-  return { bot, priority, emoji, addressed, pending, review, id, message };
-}
-
-function buildMessage(payload, requests, settings) {
-  let channel_id = payload.channel_id;
-  let channel_name = payload.channel_name;
-  let message = { unfurl_links: settings.unfurl_links };
-  let publish_test = new RegExp(settings.publish_text, 'i');
-
-  // build display text
-  let map = buildSection.bind(null, settings, requests, payload);
-  message.text = settings.display.map(map).join('\n\n\n');
-
-  // attach instructions if not publish else make public
-  if (publish_test.test(payload.text)) message.response_type = 'in_channel';
-  else message.attachments = settings.help;
-
-  return message;
-}
-
-function buildSection(settings, requests, payload, name) {
-  let channel_id = payload.channel_id;
-  let team_domain = payload.team_domain;
-  let channel_name = payload.channel_name;
-
-  // Correct the url if this comes from a DM
-  if (payload.channel_name === directMessageHandle) {
-    channel_name = payload.text;
-  }
-
-  let baseUrl = `https://${team_domain}.slack.com/archives/${channel_name}/p`;
-
-  let title = settings[name].title;
-  let filtered = requests.filter(r => r[name]);
-  let items = filtered.map(r => `:${r.emoji}: ${baseUrl + r.id}`);
-  let text = [title].concat(items).join('\n');
-
-  // replace template fields
-  text = text.replace(/{{count}}/, filtered.length);
-  text = text.replace(/{{channel}}/, `<#${channel_id}|${channel_name}>`);
-
-  return text;
-}
-
-function postResults(payload, result) {
-  return http.post(payload.response_url, result);
-}
-
-function sync_response(cb, text, only_caller) {
-  cb(null, { text: text, response_type: only_caller ? 'emphemeral' : 'in_channel' });
-}
